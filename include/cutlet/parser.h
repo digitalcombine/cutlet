846626456aa56bdbf399fd6b5bc5c22ff131ca62
Ron Wills <ron@digitalcombine.ca>
2018-12-04 20:05:54 -0600

    Fix the copyright licences.

--- include/cutlet/parser.h
+++ include/cutlet/parser.h
@@ -3,18 +3,18 @@
  *
  * This file is part of Cutlet.
  *
- * Meat is free software: you can redistribute it and/or modify
+ * Cutlet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Meat is distributed in the hope that it will be useful,
+ * Cutlet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Meat.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Cutlet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <exception>
