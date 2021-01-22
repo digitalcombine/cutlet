2bb06a202fbb474e8f8b09c26d64221b62d94a2c
Ron Wills <ron@digitalcombine.ca>
2021-01-21 21:29:48 -0600

    Update tests

--- tests/debugger-tests.cpp
+++ tests/debugger-tests.cpp
@@ -1,21 +1,43 @@
-/*
+/*                                                                 -*- c++ -*-
+ * Copyright © 2020 Ron R Wills <ron@digitalcombine.ca>
+ *
+ * This file is part of Cutlet.
+ *
+ * Cutlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cutlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cutlet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <cutlet.h>
+#include <cutlet>
 #include <iostream>
 #include <fstream>
 #include <cstring>
 
 static int results = EXIT_SUCCESS;
 
-std::ifstream script;
+static std::ifstream script;
 
-void break_int(cutlet::interpreter &interp, const cutlet::ast::node &node) {
-  //const parser::token &token = node.token();
+/*************
+ * break_int *
+ *************/
 
+static void break_int(cutlet::interpreter &interp,
+                      const cutlet::ast::node &node) {
+
+  // Initialize memory to read the script parts into.
   char *script_token = new char[node.body().size() + 3];
   std::memset(script_token, 0, node.body().size() + 3);
 
+  // With the node position read text from the script.
   switch (node.id()) {
   case cutlet::A_STRING:
   case cutlet::A_COMMENT:
@@ -56,7 +78,7 @@ void break_int(cutlet::interpreter &interp, const cutlet::ast::node &node) {
     break;
   }
 
-
+  // Is the node matching with what was read from the script.
   if (node.body() != script_token) {
 
     std::cout << "Node @ " << node.position() << ": "
@@ -85,29 +107,30 @@ void break_int(cutlet::interpreter &interp, const cutlet::ast::node &node) {
 
     std::cerr << "  " << script_token << " != " << node.body()
               << std::endl;
-    if (script_token) {
-      delete[] script_token;
-      script_token = nullptr;
-    }
-
     results = EXIT_FAILURE;
   }
 
+  // Clean up after ourself.
   if (script_token) delete[] script_token;
-
-  return;
 }
+
+/********
+ * main *
+ ********/
 
 int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
 
+  // Setup the interpreter and load the script.
   cutlet::interpreter interpreter;
   script.open("hello.cutlet");
 
+  // Setup the debugger interface.
   cutlet::ast::node::debugger(break_int);
   cutlet::ast::node::break_all = true;
 
+  // Lets get to work.
   interpreter.compile_file("hello.cutlet");
 
   return results;
